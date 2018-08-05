2f1a279556daf85b163b825bf5a1a58e3421b6b3
Nadezhda Vlasenko <nadezhda277135@gmail.com>
2018-08-06 00:20:27 +0300

    Customer, Barber, Barbershop

--- src/com/company/Barber.java
+++ src/com/company/Barber.java
@@ -1,4 +1,46 @@
 package com.company;
 
-public class Barber {
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class Barber implements Runnable{
+    private Barbershop barbershop;
+
+    @Override
+    public void run() {
+        while (true){
+            if (barbershop.noCustomers()){
+                System.out.println("Barber sleeps");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                cut(barbershop.nextCustomer());
+            }
+        }
+    }
+//
+//    private Customer customer;
+//
+    public Barber(Barbershop theShop){
+        barbershop = theShop;
+    }
+//    public void run(){
+//        while (true) {
+//            try {
+//                customer = barbershop.nextCustomer();
+//                cut();
+//                barbershop.showOut(customer);
+//            } catch (InterruptedException e){}
+//        }
+//    }
+    private void cut(Customer customer){
+        try {
+            System.out.println("barber cuts " + customer.getCustomerName());
+            Thread.sleep((int)(Math.random() * 100));
+        } catch (InterruptedException e) {}
+    }
 }
