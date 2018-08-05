d0d2e51fb85a8f1d167606851821d1379084afb5
Nadezhda Vlasenko <nadezhda277135@gmail.com>
2018-08-05 21:12:50 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+    }
+}
